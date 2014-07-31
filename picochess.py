475627fb9be5bfd5d50c52d46c5b6d9044f6a664
Jean-Francois Romang <jromang@posteo.de>
2014-07-31 21:25:02 +0200

    Add dgt board argument

--- picochess.py
+++ picochess.py
@@ -3,4 +3,5 @@
 import argparse
 
 parser = argparse.ArgumentParser()
+parser.add_argument("-d", "--dgt-port", type=str, help="enable dgt board on the given serial port such as /dev/ttyUSB0")
 parser.parse_args()
