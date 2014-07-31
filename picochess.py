48cfba8b4c02f683d4c0e4451f8eb2c66d627710
Jean-Francois Romang <jromang@posteo.de>
2014-07-31 21:50:22 +0200

    Add logging option

--- picochess.py
+++ picochess.py
@@ -1,7 +1,14 @@
 #!/usr/bin/python
 
 import argparse
+import logging
 
+#Command line argument parsing
 parser = argparse.ArgumentParser()
 parser.add_argument("-d", "--dgt-port", type=str, help="enable dgt board on the given serial port such as /dev/ttyUSB0")
-parser.parse_args()
+parser.add_argument("-l", "--log-level", choices=['notset', 'debug', 'info', 'warning', 'error', 'critical'], default='warning')
+args = parser.parse_args()
+
+#Enable logging
+logging.basicConfig(filename="picochess.log", level=getattr(logging, args.log_level.upper()))
+logging.debug("Starting picochess with DGT board on ["+args.dgt_port+"]")
