d48fcef106a758325e252028708801d30e72eb5a
Jean-Francois Romang <jromang@posteo.de>
2014-08-02 08:55:14 +0200

    UCI engine process launch

--- uci.py
+++ uci.py
@@ -2,15 +2,33 @@ import abc
 from observable import *
 import logging
 import os.path
+import spur
+import paramiko
+import subprocess
 
 
 class UCIEngine(Observable, metaclass=abc.ABCMeta):
 
-    def __init__(self, path):
+    def __init__(self, path, hostname=None, username=None, key_file=None):
         Observable.__init__(self)
         logger = logging.getLogger(__name__)
-        if not os.path.isfile(path):
-            logger.error("Engine not found [%s]", path)
+        try:
+            if hostname:
+                logger.info("Connecting to [%s]", hostname)
+                shell = spur.SshShell(
+                    hostname=hostname,
+                    username=username,
+                    private_key_file=key_file,
+                    missing_host_key=paramiko.AutoAddPolicy())
+                self.process = shell.spawn([path], stdout=subprocess.PIPE, store_pid=True, allow_error=True)
+
+            elif not os.path.isfile(path):
+                logger.error("Engine not found [%s]", path)
+                shell = spur.LocalShell()
+                self.process = shell.spawn(path, stdout=subprocess.PIPE, store_pid=True)
+        except OSError:
+            logger.exception("OS error in starting engine")
+
 
     @abc.abstractmethod
     def set_level(self, level):
