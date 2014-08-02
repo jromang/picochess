5bbfc343a6d03104793431b87df76eafbf0fc0e2
Jean-Francois Romang <jromang@posteo.de>
2014-08-02 11:13:47 +0200

    Autodetect engine executable in system PATH

--- uci.py
+++ uci.py
@@ -1,33 +1,54 @@
 import abc
 from observable import *
 import logging
-import os.path
+import os
 import spur
 import paramiko
 import subprocess
+
+
+def which(program):
+    """ Find an executable file on the system path.
+    :param program: Name or full path of the executable file
+    :return: Full path of the executable file, or None if it was not found
+    """
+    def is_exe(fpath):
+        return os.path.isfile(fpath) and os.access(fpath, os.X_OK)
+    fpath, fname = os.path.split(program)
+    if fpath:
+        if is_exe(program):
+            return program
+    else:
+        for path in os.environ["PATH"].split(os.pathsep):
+            path = path.strip('"')
+            exe_file = os.path.join(path, program)
+            if is_exe(exe_file):
+                return exe_file
+    logging.warning("Engine executable [%s] not found", program)
+    return None
 
 
 class UCIEngine(Observable, metaclass=abc.ABCMeta):
 
     def __init__(self, path, hostname=None, username=None, key_file=None):
         Observable.__init__(self)
-        logger = logging.getLogger(__name__)
         try:
             if hostname:
-                logger.info("Connecting to [%s]", hostname)
+                logging.info("Connecting to [%s]", hostname)
                 shell = spur.SshShell(
                     hostname=hostname,
                     username=username,
                     private_key_file=key_file,
                     missing_host_key=paramiko.AutoAddPolicy())
                 self.process = shell.spawn([path], stdout=subprocess.PIPE, store_pid=True, allow_error=True)
-
-            elif not os.path.isfile(path):
-                logger.error("Engine not found [%s]", path)
-                shell = spur.LocalShell()
-                self.process = shell.spawn(path, stdout=subprocess.PIPE, store_pid=True)
+            else:
+                path = which(path)
+                if not path:
+                    logging.error("Engine not found")
+                    shell = spur.LocalShell()
+                    self.process = shell.spawn(path, stdout=subprocess.PIPE, store_pid=True)
         except OSError:
-            logger.exception("OS error in starting engine")
+            logging.exception("OS error in starting engine")
 
 
     @abc.abstractmethod
