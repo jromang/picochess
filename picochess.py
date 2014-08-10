e845bfa08057763177fff079f8724183b98292b8
Jean-Francois Romang <jromang@posteo.de>
2014-08-10 19:47:24 +0200

    Create main() function

--- picochess.py
+++ picochess.py
@@ -26,173 +26,178 @@ import threading
 from utilities import *
 
 
-#Command line argument parsing
-parser = argparse.ArgumentParser()
-parser.add_argument("-d", "--dgt-port", type=str, help="enable dgt board on the given serial port such as /dev/ttyUSB0")
-parser.add_argument("-e", "--engine", type=str, help="stockfish executable path", default="/usr/bin/stockfish")
-parser.add_argument("-hs", "--hash-size", type=int, help="hashtable size in MB (default:64)", default=64)
-parser.add_argument("-t", "--threads", type=int, help="number of engine threads (default:1)", default=1)
-parser.add_argument("-l", "--log-level", choices=['notset', 'debug', 'info', 'warning', 'error', 'critical'], default='warning', help="logging level")
-parser.add_argument("-lf", "--log-file", type=str, help="log to the given file")
-parser.add_argument("-r", "--remote", type=str, help="remote server running the engine")
-parser.add_argument("-u", "--user", type=str, help="remote user on server running the engine")
-parser.add_argument("-p", "--password", type=str, help="password for the remote user")
-parser.add_argument("-k", "--key-file", type=str, help="key file used to connect to the remote server")
-parser.add_argument("-pgn", "--pgn-file", type=str, help="pgn file used to store the games")
-args = parser.parse_args()
+def main():
+    #Command line argument parsing
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-d", "--dgt-port", type=str, help="enable dgt board on the given serial port such as /dev/ttyUSB0")
+    parser.add_argument("-e", "--engine", type=str, help="stockfish executable path", default="/usr/bin/stockfish")
+    parser.add_argument("-hs", "--hash-size", type=int, help="hashtable size in MB (default:64)", default=64)
+    parser.add_argument("-t", "--threads", type=int, help="number of engine threads (default:1)", default=1)
+    parser.add_argument("-l", "--log-level", choices=['notset', 'debug', 'info', 'warning', 'error', 'critical'], default='warning', help="logging level")
+    parser.add_argument("-lf", "--log-file", type=str, help="log to the given file")
+    parser.add_argument("-r", "--remote", type=str, help="remote server running the engine")
+    parser.add_argument("-u", "--user", type=str, help="remote user on server running the engine")
+    parser.add_argument("-p", "--password", type=str, help="password for the remote user")
+    parser.add_argument("-k", "--key-file", type=str, help="key file used to connect to the remote server")
+    parser.add_argument("-pgn", "--pgn-file", type=str, help="pgn file used to store the games")
+    args = parser.parse_args()
 
-# Enable logging
-logging.basicConfig(filename=args.log_file, level=getattr(logging, args.log_level.upper()),
-                    format='%(asctime)s.%(msecs)d %(levelname)s %(module)s - %(funcName)s: %(message)s', datefmt="%Y-%m-%d %H:%M:%S")
+    # Enable logging
+    logging.basicConfig(filename=args.log_file, level=getattr(logging, args.log_level.upper()),
+                        format='%(asctime)s.%(msecs)d %(levelname)s %(module)s - %(funcName)s: %(message)s', datefmt="%Y-%m-%d %H:%M:%S")
 
-# Load UCI engine
-engine = uci.Engine(args.engine, hostname=args.remote, username=args.user, key_file=args.key_file, password=args.password)
-logging.debug('Loaded engine [%s]', engine.name)
-logging.debug('Supported options [%s]', engine.options)
-if 'Hash' in engine.options:
-    engine.set_option("Hash", args.hash_size)
-if 'Threads' in engine.options:  # Stockfish
-    engine.set_option("Threads", args.threads)
-if 'Core Threads' in engine.options:  # Hiarcs
-    engine.set_option("Core Threads", args.threads)
+    # Load UCI engine
+    engine = uci.Engine(args.engine, hostname=args.remote, username=args.user, key_file=args.key_file, password=args.password)
+    logging.debug('Loaded engine [%s]', engine.name)
+    logging.debug('Supported options [%s]', engine.options)
+    if 'Hash' in engine.options:
+        engine.set_option("Hash", args.hash_size)
+    if 'Threads' in engine.options:  # Stockfish
+        engine.set_option("Threads", args.threads)
+    if 'Core Threads' in engine.options:  # Hiarcs
+        engine.set_option("Core Threads", args.threads)
 
-# Connect to DGT board
-board = None
-if args.dgt_port:
-    logging.debug("Starting picochess with DGT board on [%s]", args.dgt_port)
-    board = dgtv2.DGTBoard(args.dgt_port)
-    board.start()
-else:
-    logging.warning("No DGT board port provided")
-
-
-def compute_legal_fens(g):
-    """
-    Computes a list of legal FENs for the given game.
-    Also stores the initial position in the 'root' attribute.
-    :param g: The game
-    :return: A list of legal FENs, and the root FEN
-    """
-    class FenList(list):
-        def __init__(self, *args):
-            list.__init__(self, *args)
-            self.root = ''
-
-    fens = FenList()
-    for move in g.generate_legal_moves():
-        g.push(move)
-        fens.append(g.fen().split(' ')[0])
-        g.pop()
-    fens.root = g.fen().split(' ')[0]
-    return fens
-
-
-def think():
-    """
-    Starts a new search on the current game.
-    If a move is found in the opening book, fire an event in a few seconds.
-    :return:
-    """
-    def send_book_move(move):
-        Observable.fire(Event.BEST_MOVE, move.uci())
-
-    global book_thread
-    book_move = weighted_choice(book, game)
-    if book_move:
-        Display.show(Message.BOOK_MOVE, book_move.uci())
-        book_thread = threading.Timer(2, send_book_move, [book_move])
-        book_thread.start()
+    # Connect to DGT board
+    board = None
+    if args.dgt_port:
+        logging.debug("Starting picochess with DGT board on [%s]", args.dgt_port)
+        board = dgtv2.DGTBoard(args.dgt_port)
+        board.start()
     else:
-        book_thread = None
-        engine.set_position(game)
-        engine.go()
-        Display.show(Message.SEARCH_STARTED)
+        logging.warning("No DGT board port provided")
 
 
-def stop_thinking():
-    """
-    Stop current search or book thread.
-    :return:
-    """
-    if book_thread:
-        book_thread.cancel()
-    else:
-        engine.stop(True)
+    def compute_legal_fens(g):
+        """
+        Computes a list of legal FENs for the given game.
+        Also stores the initial position in the 'root' attribute.
+        :param g: The game
+        :return: A list of legal FENs, and the root FEN
+        """
+        class FenList(list):
+            def __init__(self, *args):
+                list.__init__(self, *args)
+                self.root = ''
 
-game = chess.Bitboard()  # Create the current game
-legal_fens = compute_legal_fens(game)  # Compute the legal FENs
-book = chess.polyglot.open_reader(get_opening_books()[8][1])  # Default opening book
-interaction_mode = Mode.PLAY_WHITE   # Interaction mode
-book_thread = None  # The thread that will fire book moves.
+        fens = FenList()
+        for move in g.generate_legal_moves():
+            g.push(move)
+            fens.append(g.fen().split(' ')[0])
+            g.pop()
+        fens.root = g.fen().split(' ')[0]
+        return fens
 
-#Event loop
-while True:
-    event = event_queue.get()
-    logging.debug('Received event in event loop : %s', event)
 
-    for case in switch(event):
+    def think():
+        """
+        Starts a new search on the current game.
+        If a move is found in the opening book, fire an event in a few seconds.
+        :return:
+        """
+        def send_book_move(move):
+            Observable.fire(Event.BEST_MOVE, move.uci())
 
-        if case(Event.FEN):  # User sets a new position, convert it to a move if it is legal
-            if event.parameter in legal_fens:
-                # Check if we have to undo a previous move (sliding)
-                if (interaction_mode == Mode.PLAY_WHITE and game.turn == chess.BLACK) or (interaction_mode == Mode.PLAY_BLACK and game.turn == chess.WHITE):
+        global book_thread
+        book_move = weighted_choice(book, game)
+        if book_move:
+            Display.show(Message.BOOK_MOVE, book_move.uci())
+            book_thread = threading.Timer(2, send_book_move, [book_move])
+            book_thread.start()
+        else:
+            book_thread = None
+            engine.set_position(game)
+            engine.go()
+            Display.show(Message.SEARCH_STARTED)
+
+
+    def stop_thinking():
+        """
+        Stop current search or book thread.
+        :return:
+        """
+        if book_thread:
+            book_thread.cancel()
+        else:
+            engine.stop(True)
+
+    game = chess.Bitboard()  # Create the current game
+    legal_fens = compute_legal_fens(game)  # Compute the legal FENs
+    book = chess.polyglot.open_reader(get_opening_books()[8][1])  # Default opening book
+    interaction_mode = Mode.PLAY_WHITE   # Interaction mode
+    book_thread = None  # The thread that will fire book moves.
+
+    #Event loop
+    while True:
+        event = event_queue.get()
+        logging.debug('Received event in event loop : %s', event)
+
+        for case in switch(event):
+
+            if case(Event.FEN):  # User sets a new position, convert it to a move if it is legal
+                if event.parameter in legal_fens:
+                    # Check if we have to undo a previous move (sliding)
+                    if (interaction_mode == Mode.PLAY_WHITE and game.turn == chess.BLACK) or (interaction_mode == Mode.PLAY_BLACK and game.turn == chess.WHITE):
+                        stop_thinking()
+                        game.pop()
+                    legal_moves = list(game.generate_legal_moves())
+                    Observable.fire(Event.USER_MOVE, legal_moves[legal_fens.index(event.parameter)])
+                elif event.parameter == game.fen().split(' ')[0]:  # Player had done the computer move on the board
+                    Display.show(Message.COMPUTER_MOVE_DONE_ON_BOARD)
+                elif event.parameter == legal_fens.root:  # Allow user to take his move back while the engine is searching
                     stop_thinking()
                     game.pop()
-                legal_moves = list(game.generate_legal_moves())
-                Observable.fire(Event.USER_MOVE, legal_moves[legal_fens.index(event.parameter)])
-            elif event.parameter == game.fen().split(' ')[0]:  # Player had done the computer move on the board
-                Display.show(Message.COMPUTER_MOVE_DONE_ON_BOARD)
-            elif event.parameter == legal_fens.root:  # Allow user to take his move back while the engine is searching
-                stop_thinking()
-                game.pop()
-                Display.show(Message.USER_TAKE_BACK)
-            break
+                    Display.show(Message.USER_TAKE_BACK)
+                break
 
-        if case(Event.USER_MOVE):  # User sends a new move
-            move = event.parameter
-            logging.debug('User move [%s]', move)
-            if not move in game.generate_legal_moves():
-                logging.warning('Illegal move [%s]', move)
-            # Check if we are in play mode and it is player's turn
-            if (interaction_mode == Mode.PLAY_WHITE and game.turn == chess.WHITE) or (interaction_mode == Mode.PLAY_BLACK and game.turn == chess.BLACK):
-                game.push(move)
-                think()
-            break
+            if case(Event.USER_MOVE):  # User sends a new move
+                move = event.parameter
+                logging.debug('User move [%s]', move)
+                if not move in game.generate_legal_moves():
+                    logging.warning('Illegal move [%s]', move)
+                # Check if we are in play mode and it is player's turn
+                if (interaction_mode == Mode.PLAY_WHITE and game.turn == chess.WHITE) or (interaction_mode == Mode.PLAY_BLACK and game.turn == chess.BLACK):
+                    game.push(move)
+                    think()
+                break
 
-        if case(Event.LEVEL):  # User sets a new level
-            level = event.parameter
-            logging.debug("Setting engine to level %i", level)
-            engine.set_level(level)
-            break
+            if case(Event.LEVEL):  # User sets a new level
+                level = event.parameter
+                logging.debug("Setting engine to level %i", level)
+                engine.set_level(level)
+                break
 
-        if case(Event.NEW_GAME):  # User starts a new game
-            if game.move_stack:
-                logging.debug("Starting a new game")
-                game = chess.Bitboard()
-                legal_fens = compute_legal_fens(game)
-                Display.show(Message.START_NEW_GAME)
-            if interaction_mode == Mode.PLAY_BLACK:
-                think()
-            break
+            if case(Event.NEW_GAME):  # User starts a new game
+                if game.move_stack:
+                    logging.debug("Starting a new game")
+                    game = chess.Bitboard()
+                    legal_fens = compute_legal_fens(game)
+                    Display.show(Message.START_NEW_GAME)
+                if interaction_mode == Mode.PLAY_BLACK:
+                    think()
+                break
 
-        if case(Event.OPENING_BOOK):
-            logging.debug("Changing opening book [%s]", get_opening_books()[event.parameter][1])
-            book = chess.polyglot.open_reader(get_opening_books()[event.parameter][1])
-            break
+            if case(Event.OPENING_BOOK):
+                logging.debug("Changing opening book [%s]", get_opening_books()[event.parameter][1])
+                book = chess.polyglot.open_reader(get_opening_books()[event.parameter][1])
+                break
 
-        if case(Event.BEST_MOVE):
-            move = chess.Move.from_uci(event.parameter)
-            # Check if we are in play mode and it is computer's turn
-            if (interaction_mode == Mode.PLAY_WHITE and game.turn == chess.BLACK) or (interaction_mode == Mode.PLAY_BLACK and game.turn == chess.WHITE):
-                game.push(move)
-                legal_fens = compute_legal_fens(game)
-                Display.show(Message.COMPUTER_MOVE, move.uci())
-            break
+            if case(Event.BEST_MOVE):
+                move = chess.Move.from_uci(event.parameter)
+                # Check if we are in play mode and it is computer's turn
+                if (interaction_mode == Mode.PLAY_WHITE and game.turn == chess.BLACK) or (interaction_mode == Mode.PLAY_BLACK and game.turn == chess.WHITE):
+                    game.push(move)
+                    legal_fens = compute_legal_fens(game)
+                    Display.show(Message.COMPUTER_MOVE, move.uci())
+                break
 
-        if case(Event.SET_MODE):
-            Display.show(Message.INTERACTION_MODE, event.parameter)  # Usefull for pgn display device
-            interaction_mode = event.parameter
-            break
+            if case(Event.SET_MODE):
+                Display.show(Message.INTERACTION_MODE, event.parameter)  # Usefull for pgn display device
+                interaction_mode = event.parameter
+                break
 
-        if case():  # Default
-            logging.warning("Event not handled : [%s]", event)
+            if case():  # Default
+                logging.warning("Event not handled : [%s]", event)
+
+
+if __name__ == '__main__':
+    main()
