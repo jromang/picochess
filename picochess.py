3a4c1322f2f2446eea30e6a64be6496a3c569ef5
Jean-Francois Romang <jromang@posteo.de>
2014-08-23 07:51:11 +0200

    Change sys.path

--- picochess.py
+++ picochess.py
@@ -15,7 +15,9 @@
 #
 # You should have received a copy of the GNU General Public License
 # along with this program. If not, see <http://www.gnu.org/licenses/>.
-
+import sys
+import os
+sys.path.insert(0, os.path.dirname(os.path.realpath(__file__))+os.sep+'libs')
 import argparse
 import chess
 import chess.polyglot
