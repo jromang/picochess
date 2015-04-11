7db2ccbc1ffc45ba12c30b64620c538c634462de
Niklas Fiekas <niklas.fiekas@tu-clausthal.de>
2015-04-11 13:56:56 +0200

    Add shebang on chesstalker.py

--- libs/chesstalker/chesstalker.py
+++ libs/chesstalker/chesstalker.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+
 # Copyright (C) 2013-2015 Jean-Francois Romang (jromang@posteo.de)
 #                         Shivkumar Shivaji ()
 #                         Charles Gamble ()
