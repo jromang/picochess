60bba0bffe75e955508d791985450b43db29be3a
Jürgen Précour <info@keyman-versicherung.info>
2015-06-30 16:09:51 +0200

    Merge branch 'master' of https://github.com/jromang/picochess
    Conflicts:
    	dgt.py

--- dgt.py
+++ dgt.py
@@ -64,7 +64,7 @@ class Commands(enum.Enum):
     DGT_RETURN_LONG_SERIALNR = 0x55  # Results in a DGT_LONG_SERIALNR message
     DGT_SET_LEDS = 0x60  # Only for the Revelation II to switch a LED pattern on. This is a command that
                          # has three extra bytes with data.
-    #Clock commands, returns ACK message if mode is in UPDATE or UPDATE_NICE
+    # Clock commands, returns ACK message if mode is in UPDATE or UPDATE_NICE
     DGT_CLOCK_MESSAGE = 0x2b  # This message contains a command for the clock.
 
 
@@ -545,27 +545,7 @@ class DGTBoard(Observable, Display, threading.Thread):
                         self.process_button0()
                     if 33 <= message[4] <= 34 and message[5] == 52:
                         logging.info("Button 1 pressed")
-                        if self.dgt_clock_menu == Menu.GAME_MENU:
-                            if self.display_move:
-                                if self.hint_fen is None:
-                                    self.display_on_dgt_clock('none')
-                                else:
-                                    self.display_move_on_dgt(self.hint_move, self.hint_fen, self.enable_dgt_clock_beep)
-                            else:
-                                if self.mate is None:
-                                    sc = 'none' if self.score is None else str(self.score).rjust(6)
-                                else:
-                                    sc = 'm ' + str(self.mate)
-                                self.display_on_dgt_clock(sc, beep=True)
-                            self.display_move = not self.display_move
-
-                        if self.dgt_clock_menu == Menu.SETUP_POSITION_MENU:
-                            self.setup_reverse_orientation = not self.setup_reverse_orientation
-                            orientation = "b    w" if self.setup_reverse_orientation else "w    b"
-                            self.display_on_dgt_xl(orientation, beep=True)
-                            orientation = "b      w" if self.setup_reverse_orientation else "w      b"
-                            self.display_on_dgt_3000(orientation, beep=True)
-
+                        self.process_button1()
                     if 17 <= message[4] <= 18 and message[5] == 51:
                         logging.info("Button 2 pressed")
                         self.process_button2()
