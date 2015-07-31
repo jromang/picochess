d20cc2f6e35b3d3e83066f26ea49295f39a424d0
Jürgen Precour <info@keyman-versicherung.info>
2015-07-31 21:31:19 +0200

    new Idea with DGT communication (first try)

--- dgthardware.py
+++ dgthardware.py
@@ -17,6 +17,7 @@
 import logging
 import chess
 from dgtinterface import *
+from dgtserial import *
 
 try:
     import enum
@@ -74,11 +75,13 @@ class Clock(enum.Enum):
     DGT_CMD_CLOCK_END_MESSAGE = 0x00
 
 
-class DGTHardware(DGTInterface):
+class DGTHardware(DGTInterface, DGTSerial):
     def __init__(self, device, enable_board_leds, enable_dgt_3000, disable_dgt_clock_beep):
         super(DGTHardware, self).__init__(device, enable_board_leds, enable_dgt_3000, disable_dgt_clock_beep)
         self.displayed_text = None  # The current clock display or None if in ClockNRun mode or unknown text
         self.clock_found = True
+        # does that work?!?
+        DGTSerial(device, enable_dgt_3000).start()
 
     def _display_on_dgt_xl(self, text, beep=False):
         if self.clock_found and not self.enable_dgt_3000:
