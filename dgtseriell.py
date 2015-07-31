ae0c43b1c4fd77668e8c6618e924ad6026ef73e2
Jürgen Précour <info@keyman-versicherung.info>
2015-07-31 16:39:38 +0200

    new try

---
+++ dgtseriell.py
@@ -0,0 +1,391 @@
+# Copyright (C) 2013-2014 Jean-Francois Romang (jromang@posteo.de)
+#                         Shivkumar Shivaji ()
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+import logging
+import queue
+import serial as pyserial
+import chess
+import time
+
+from timecontrol import *
+from struct import unpack
+from collections import OrderedDict
+from utilities import *
+from subprocess import Popen
+
+try:
+    import enum
+except ImportError:  # Python 3.3 support
+    import enum34 as enum
+try:
+    import asyncio
+except ImportError:  # Python 3.3 support
+    import asyncio34 as asyncio
+
+
+@enum.unique
+class Commands(enum.Enum):
+    """ COMMAND CODES FROM PC TO BOARD """
+    # Commands not resulting in returning messages:
+    DGT_SEND_RESET = 0x40  # Puts the board in IDLE mode, cancelling any UPDATE mode
+    DGT_STARTBOOTLOADER = 0x4e  # Makes a long jump to the FC00 boot loader code. Start FLIP now
+    # Commands resulting in returning message(s):
+    DGT_SEND_CLK = 0x41  # Results in a DGT_MSG_BWTIME message
+    DGT_SEND_BRD = 0x42  # Results in a DGT_MSG_BOARD_DUMP message
+    DGT_SEND_UPDATE = 0x43  # Results in DGT_MSG_FIELD_UPDATE messages and DGT_MSG_BWTIME messages
+    # as long as the board is in UPDATE mode
+    DGT_SEND_UPDATE_BRD = 0x44  # Results in DGT_MSG_FIELD_UPDATE messages as long as the board is in UPDATE_BOARD mode
+    DGT_RETURN_SERIALNR = 0x45  # Results in a DGT_MSG_SERIALNR message
+    DGT_RETURN_BUSADRES = 0x46  # Results in a DGT_MSG_BUSADRES message
+    DGT_SEND_TRADEMARK = 0x47  # Results in a DGT_MSG_TRADEMARK message
+    DGT_SEND_EE_MOVES = 0x49  # Results in a DGT_MSG_EE_MOVES message
+    DGT_SEND_UPDATE_NICE = 0x4b  # Results in DGT_MSG_FIELD_UPDATE messages and DGT_MSG_BWTIME messages,
+    # the latter only at time changes, as long as the board is in UPDATE_NICE mode
+    DGT_SEND_BATTERY_STATUS = 0x4c  # New command for bluetooth board. Requests the battery status from the board.
+    DGT_SEND_VERSION = 0x4d  # Results in a DGT_MSG_VERSION message
+    DGT_SEND_BRD_50B = 0x50  # Results in a DGT_MSG_BOARD_DUMP_50 message: only the black squares
+    DGT_SCAN_50B = 0x51  # Sets the board in scanning only the black squares. This is written in EEPROM
+    DGT_SEND_BRD_50W = 0x52  # Results in a DGT_MSG_BOARD_DUMP_50 message: only the black squares
+    DGT_SCAN_50W = 0x53  # Sets the board in scanning only the black squares. This is written in EEPROM.
+    DGT_SCAN_100 = 0x54  # Sets the board in scanning all squares. This is written in EEPROM
+    DGT_RETURN_LONG_SERIALNR = 0x55  # Results in a DGT_LONG_SERIALNR message
+    DGT_SET_LEDS = 0x60  # Only for the Revelation II to switch a LED pattern on. This is a command that
+    # has three extra bytes with data.
+    # Clock commands, returns ACK message if mode is in UPDATE or UPDATE_NICE
+    DGT_CLOCK_MESSAGE = 0x2b  # This message contains a command for the clock.
+
+
+class Clock(enum.Enum):
+    DGT_CMD_CLOCK_DISPLAY = 0x01  # This command can control the segments of six 7-segment characters,
+    # two dots, two semicolons and the two '1' symbols.
+    DGT_CMD_CLOCK_ICONS = 0x02  # Used to control the clock icons like flags etc.
+    DGT_CMD_CLOCK_END = 0x03  # This command clears the message and brings the clock back to the
+    # normal display (showing clock times).
+    DGT_CMD_CLOCK_BUTTON = 0x08  # Requests the current button pressed (if any).
+    DGT_CMD_CLOCK_VERSION = 0x09  # This commands requests the clock version.
+    DGT_CMD_CLOCK_SETNRUN = 0x0a  # This commands controls the clock times and counting direction, when
+    # the clock is in mode 23. A clock can be paused or counting down. But
+    # counting up isn't supported on current DGT XL's (1.14 and lower) yet.
+    DGT_CMD_CLOCK_BEEP = 0x0b  # This clock command turns the beep on, for a specified time (64ms * byte 5)
+    DGT_CMD_CLOCK_ASCII = 0x0c  # This clock commands sends a ASCII message to the clock that
+    # can be displayed only by the DGT3000.
+    DGT_CMD_CLOCK_START_MESSAGE = 0x03
+    DGT_CMD_CLOCK_END_MESSAGE = 0x00
+
+
+@enum.unique
+class Pieces(enum.Enum):
+    # Piece codes for chess pieces:
+    EMPTY = 0x00
+    WPAWN = 0x01
+    WROOK = 0x02
+    WKNIGHT = 0x03
+    WBISHOP = 0x04
+    WKING = 0x05
+    WQUEEN = 0x06
+    BPAWN = 0x07
+    BROOK = 0x08
+    BKNIGHT = 0x09
+    BBISHOP = 0x0a
+    BKING = 0x0b
+    BQUEEN = 0x0c
+    PIECE1 = 0x0d  # Magic piece: Draw
+    PIECE2 = 0x0e  # Magic piece: White win
+    PIECE3 = 0x0f  # Magic piece: Black win
+
+
+class Messages(enum.IntEnum):
+    """
+    DESCRIPTION OF THE MESSAGES FROM BOARD TO PC
+    """
+    MESSAGE_BIT = 0x80  # The Message ID is the logical OR of MESSAGE_BIT and ID code
+    # ID codes
+    DGT_NONE = 0x00
+    DGT_BOARD_DUMP = 0x06
+    DGT_BWTIME = 0x0d
+    DGT_FIELD_UPDATE = 0x0e
+    DGT_EE_MOVES = 0x0f
+    DGT_BUSADRES = 0x10
+    DGT_SERIALNR = 0x11
+    DGT_TRADEMARK = 0x12
+    DGT_VERSION = 0x13
+    DGT_BOARD_DUMP_50B = 0x14  # Added for Draughts board
+    DGT_BOARD_DUMP_50W = 0x15  # Added for Draughts board
+    DGT_BATTERY_STATUS = 0x20  # Added for Bluetooth board
+    DGT_LONG_SERIALNR = 0x22  # Added for Bluetooth board
+    # DGT_MSG_BOARD_DUMP is the message that follows on a DGT_SEND_BOARD command
+    DGT_MSG_BOARD_DUMP = (MESSAGE_BIT | DGT_BOARD_DUMP)
+    DGT_SIZE_BOARD_DUMP = 67
+    DGT_SIZE_BOARD_DUMP_DRAUGHTS = 103
+    DGT_MSG_BOARD_DUMP_50B = (MESSAGE_BIT | DGT_BOARD_DUMP_50B)
+    DGT_SIZE_BOARD_DUMP_50B = 53
+    DGT_MSG_BOARD_DUMP_50W = (MESSAGE_BIT | DGT_BOARD_DUMP_50W)
+    DGT_SIZE_BOARD_DUMP_50W = 53
+    DGT_MSG_BWTIME = (MESSAGE_BIT | DGT_BWTIME)
+    DGT_SIZE_BWTIME = 10
+    DGT_MSG_FIELD_UPDATE = (MESSAGE_BIT | DGT_FIELD_UPDATE)
+    DGT_SIZE_FIELD_UPDATE = 5
+    DGT_MSG_TRADEMARK = (MESSAGE_BIT | DGT_TRADEMARK)
+    DGT_MSG_BUSADRES = (MESSAGE_BIT | DGT_BUSADRES)
+    DGT_SIZE_BUSADRES = 5
+    DGT_MSG_SERIALNR = (MESSAGE_BIT | DGT_SERIALNR)
+    DGT_SIZE_SERIALNR = 8
+    DGT_MSG_LONG_SERIALNR = (MESSAGE_BIT | DGT_LONG_SERIALNR)
+    DGT_SIZE_LONG_SERIALNR = 13
+    DGT_MSG_VERSION = (MESSAGE_BIT | DGT_VERSION)
+    DGT_SIZE_VERSION = 5
+    DGT_MSG_BATTERY_STATUS = (MESSAGE_BIT | DGT_BATTERY_STATUS)
+    DGT_SIZE_BATTERY_STATUS = 7
+    DGT_MSG_EE_MOVES = (MESSAGE_BIT | DGT_EE_MOVES)
+    # Definition of the one-byte EEPROM message codes
+    EE_POWERUP = 0x6a
+    EE_EOF = 0x6b
+    EE_FOURROWS = 0x6c
+    EE_EMPTYBOARD = 0x6d
+    EE_DOWNLOADED = 0x6e
+    EE_BEGINPOS = 0x6f
+    EE_BEGINPOS_ROT = 0x7a
+    EE_START_TAG = 0x7b
+    EE_WATCHDOG_ACTION = 0x7c
+    EE_FUTURE_1 = 0x7d
+    EE_FUTURE_2 = 0x7e
+    EE_NOP = 0x7f
+    EE_NOP2 = 0x00
+
+
+char_to_DGTXL = {
+    '0': 0x01 | 0x02 | 0x20 | 0x08 | 0x04 | 0x10, '1': 0x02 | 0x04, '2': 0x01 | 0x40 | 0x08 | 0x02 | 0x10,
+    '3': 0x01 | 0x40 | 0x08 | 0x02 | 0x04, '4': 0x20 | 0x04 | 0x40 | 0x02, '5': 0x01 | 0x40 | 0x08 | 0x20 | 0x04,
+    '6': 0x01 | 0x40 | 0x08 | 0x20 | 0x04 | 0x10, '7': 0x02 | 0x04 | 0x01,
+    '8': 0x01 | 0x02 | 0x20 | 0x40 | 0x04 | 0x10 | 0x08, '9': 0x01 | 0x40 | 0x08 | 0x02 | 0x04 | 0x20,
+    'a': 0x01 | 0x02 | 0x20 | 0x40 | 0x04 | 0x10, 'b': 0x20 | 0x04 | 0x40 | 0x08 | 0x10, 'c': 0x01 | 0x20 | 0x10 | 0x08,
+    'd': 0x10 | 0x40 | 0x08 | 0x02 | 0x04, 'e': 0x01 | 0x40 | 0x08 | 0x20 | 0x10, 'f': 0x01 | 0x40 | 0x20 | 0x10,
+    'g': 0x01 | 0x20 | 0x10 | 0x08 | 0x04, 'h': 0x20 | 0x10 | 0x04 | 0x40, 'i': 0x02 | 0x04,
+    'j': 0x02 | 0x04 | 0x08 | 0x10, 'k': 0x01 | 0x20 | 0x40 | 0x04 | 0x10, 'l': 0x20 | 0x10 | 0x08,
+    'm': 0x01 | 0x40 | 0x04 | 0x10, 'n': 0x40 | 0x04 | 0x10, 'o': 0x40 | 0x04 | 0x10 | 0x08,
+    'p': 0x01 | 0x40 | 0x20 | 0x10 | 0x02, 'q': 0x01 | 0x40 | 0x20 | 0x04 | 0x02, 'r': 0x40 | 0x10,
+    's': 0x01 | 0x40 | 0x08 | 0x20 | 0x04, 't': 0x20 | 0x10 | 0x08 | 0x40, 'u': 0x08 | 0x02 | 0x20 | 0x04 | 0x10,
+    'v': 0x08 | 0x02 | 0x20, 'w': 0x40 | 0x08 | 0x20 | 0x02, 'x': 0x20 | 0x10 | 0x04 | 0x40 | 0x02,
+    'y': 0x20 | 0x08 | 0x04 | 0x40 | 0x02, 'z': 0x01 | 0x40 | 0x08 | 0x02 | 0x10, ' ': 0x00, '-': 0x40
+}
+
+piece_to_char = {
+    0x01: 'P', 0x02: 'R', 0x03: 'N', 0x04: 'B', 0x05: 'K', 0x06: 'Q',
+    0x07: 'p', 0x08: 'r', 0x09: 'n', 0x0a: 'b', 0x0b: 'k', 0x0c: 'q', 0x00: '.'
+}
+
+
+# reads from DGT queue (HardwareDisplay), and writes to Message.DGT_FEN & Message.DGT_BUTTON (Display)
+class DGTSeriell(Display, HardwareDisplay, threading.Thread):
+    def __init__(self, device, enable_board_leds, enable_dgt_3000, disable_dgt_clock_beep):
+        super(DGTSeriell, self).__init__()
+
+        self.serial_queue = queue.Queue()
+        self.clock_lock = asyncio.Lock()
+        self.enable_dgt_3000 = enable_dgt_3000
+        self.enable_board_leds = enable_board_leds
+        self.disable_dgt_clock_beep = disable_dgt_clock_beep
+        self.displayed_text = None  # The current clock display or None if in ClockNRun mode or unknown text
+
+        # Open the serial port
+        try:
+            self.serial = pyserial.Serial(device, stopbits=pyserial.STOPBITS_ONE,
+                                          parity=pyserial.PARITY_NONE,
+                                          bytesize=pyserial.EIGHTBITS,
+                                          timeout=2
+                                          )
+        except pyserial.SerialException as e:
+            logging.warning(e)
+
+        # Set the board update mode
+        self.serial.write(bytearray([Commands.DGT_SEND_UPDATE_NICE.value]))
+        # Detect DGT XL clock
+        self.clock_found = False
+        # we sending a beep command, and see if its ack'ed
+        self.serial.write(bytearray([0x2b, 0x04, 0x03, 0x0b, 1, 0x00]))
+        time.sleep(1)
+        # logging.debug('DGT clock found' if self.clock_found else 'DGT clock NOT found')
+
+        # Get clock version
+        self.write([Commands.DGT_CLOCK_MESSAGE, 0x03, Clock.DGT_CMD_CLOCK_START_MESSAGE,
+                    Clock.DGT_CMD_CLOCK_VERSION, Clock.DGT_CMD_CLOCK_END_MESSAGE])
+        # Get board version
+        self.board_version = 0.0
+        self.write([Commands.DGT_SEND_VERSION])
+        # Update the board
+        self.write([Commands.DGT_SEND_BRD])
+
+    def write(self, message):
+        self.serial_queue.put(message)
+
+    def send_command(self, message):
+        mes = message[3] if message[0] == Commands.DGT_CLOCK_MESSAGE else message[0]
+        logging.debug('->DGT [%s]', mes)
+        array = []
+        for v in message:
+            if type(v) is int:
+                array.append(v)
+            elif isinstance(v, enum.Enum):
+                array.append(v.value)
+            elif type(v) is str:
+                for c in v:
+                    array.append(char_to_DGTXL[c])
+            else:
+                logging.error('Type not supported : [%s]', type(v))
+        try:
+            self.serial.write(bytearray(array))
+        except ValueError:
+            logging.error('Invalid bytes sent {0}'.format(array))
+        if message[0] == Commands.DGT_CLOCK_MESSAGE:
+            # Let a bit time for the message to be displayed on the clock
+            time.sleep(0.05 if self.enable_dgt_3000 else 0.5)
+            self.clock_lock.acquire()
+
+    def process_message(self, message_id, message):
+        for case in switch(message_id):
+            if case(Messages.DGT_MSG_VERSION):  # Get the DGT board version
+                self.board_version = float(str(message[0]) + '.' + str(message[1]))
+                logging.debug("DGT board version %0.2f", self.board_version)
+                break
+            # if case():
+            #     logging.info("Got clock version number")
+            #     break
+            if case(Messages.DGT_MSG_BWTIME):
+                if ((message[0] & 0x0f) == 0x0a) or ((message[3] & 0x0f) == 0x0a):  # Clock ack message
+                    # Construct the ack message
+                    ack0 = ((message[1]) & 0x7f) | ((message[3] << 3) & 0x80)
+                    ack1 = ((message[2]) & 0x7f) | ((message[3] << 2) & 0x80)
+                    ack2 = ((message[4]) & 0x7f) | ((message[0] << 3) & 0x80)
+                    ack3 = ((message[5]) & 0x7f) | ((message[0] << 2) & 0x80)
+                    if ack1 == 0x88:
+                        # this are the other (ack2-ack3) codes
+                        # 6-49 34-52 18-51 10-50 66-53 | button 0-4 (single)
+                        #      38-52 22-51 14-50 70-53 | button 0 + 1-4
+                        #            50-51 42-50 98-53 | button 1 + 2-4
+                        #                  26-50 82-53 | button 2 + 3-4
+                        #                        74-53 | button 3 + 4
+                        if ack3 == 49:
+                            logging.info("Button 0 pressed")
+                            Display.show(Message.DGT_BUTTON, button=0)
+                        if ack3 == 52:
+                            logging.info("Button 1 pressed")
+                            Display.show(Message.DGT_BUTTON, button=1)
+                        if ack3 == 51:
+                            logging.info("Button 2 pressed")
+                            Display.show(Message.DGT_BUTTON, button=2)
+                        if ack3 == 50:
+                            logging.info("Button 3 pressed")
+                            Display.show(Message.DGT_BUTTON, button=3)
+                        if ack3 == 53:
+                            logging.info("Button 4 pressed")
+                            Display.show(Message.DGT_BUTTON, button=4)
+                    if ack1 == 0x09:  # we using the beep command, to find out if a clock is there
+                        self.clock_found = True
+                        main_version = ack2 >> 4
+                        sub_version = ack2 & 0x0f
+                        logging.debug("DGT clock version %0.2f", float(str(main_version) + '.' + str(sub_version)))
+                        if main_version == 2:
+                            self.enable_dgt_3000 = True
+                        self.display_text_on_clock('pico ' + version, 'pic' + version, beep=BeepLevel.YES)
+                    if ack0 != 0x10:
+                        logging.warning("Clock ACK error %s", (ack0, ack1, ack2, ack3))
+                    else:
+                        logging.debug("Clock ACK %s", (ack0, ack1, ack2, ack3))
+                        if self.clock_lock.locked():
+                            self.clock_lock.release()
+                        return None
+                elif any(message[:6]):
+                    self.displayed_text = None  # reset saved text to unknown
+                    r_hours = message[0] & 0x0f
+                    r_mins = (message[1] >> 4) * 10 + (message[1] & 0x0f)
+                    r_secs = (message[2] >> 4) * 10 + (message[1] & 0x0f)
+                    l_hours = message[3] & 0x0f
+                    l_mins = (message[4] >> 4) * 10 + (message[4] & 0x0f)
+                    l_secs = (message[5] >> 4) * 10 + (message[5] & 0x0f)
+                    logging.info(
+                        'DGT clock time received {} : {}'.format((l_hours, l_mins, l_secs), (r_hours, r_mins, r_secs)))
+                break
+            if case(Messages.DGT_MSG_BOARD_DUMP):
+                board = ''
+                for c in message:
+                    board += piece_to_char[c]
+                logging.debug('\n' + '\n'.join(board[0 + i:8 + i] for i in range(0, len(board), 8)))  # Show debug board
+                # Create fen from board
+                fen = ''
+                empty = 0
+                for sq in range(0, 64):
+                    if message[sq] != 0:
+                        if empty > 0:
+                            fen += str(empty)
+                            empty = 0
+                        fen += piece_to_char[message[sq]]
+                    else:
+                        empty += 1
+                    if (sq + 1) % 8 == 0:
+                        if empty > 0:
+                            fen += str(empty)
+                            empty = 0
+                        if sq < 63:
+                            fen += "/"
+                        empty = 0
+
+                # Now we have a FEN -> fire a fen-event with it
+                # Attention: This fen is NOT flipped!!
+                logging.debug("Fen")
+                Display.show(Message.DGT_FEN, fen=fen)
+                break
+            if case(Messages.DGT_MSG_FIELD_UPDATE):
+                self.write([Commands.DGT_SEND_BRD])  # Ask for the board when a piece moved
+                break
+            if case():  # Default
+                logging.warning("DGT message not handled : [%s]", Messages(message_id))
+
+    def read_message(self, head = None):
+        header_len = 3
+        if head:
+            header = head + self.serial.read(header_len-1)
+        else:
+            header = self.serial.read(header_len)
+
+        pattern = '>'+'B'*header_len
+        header = unpack(pattern, header)
+
+        # header = unpack('>BBB', (self.serial.read(3)))
+        message_id = header[0]
+        message_length = (header[1] << 7) + header[2] - 3
+
+        try:
+            logging.debug("<-DGT [%s], length:%i", Messages(message_id), message_length)
+        except ValueError:
+            logging.warning("Unknown message value %i", message_id)
+        if message_length:
+            message = unpack('>' + str(message_length) + 'B', (self.serial.read(message_length)))
+            self.process_message(message_id, message)
+            return message_id
+
+    def run(self):
+        while True:
+            # Check if we have a message from the board
+            c = self.serial.read(1)
+            if c:
+                self.read_message(head=c)
+            if not self.clock_lock.locked():
+                # Check if we have something to send
+                try:
+                    command = self.seriel_queue.get_nowait()
+                    self.send_command(command)
+                except queue.Empty:
+                    pass
