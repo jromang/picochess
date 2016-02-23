3dceb619bc3d395ad7ce3e90af456c1f7402f37d
Jürgen Précour <info@keyman-versicherung.info>
2016-02-23 13:20:33 +0100

    Copyright Update

--- picochess.py
+++ picochess.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 
-# Copyright (C) 2013-2014 Jean-Francois Romang (jromang@posteo.de)
+# Copyright (C) 2013-2016 Jean-Francois Romang (jromang@posteo.de)
 #                         Shivkumar Shivaji ()
+#                         Jürgen Précour (LocutusOfPenguin@posteo.de)
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
