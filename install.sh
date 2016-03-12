656807baef574ae0fe6d7d16c7b662327a51dd1b
LaMa Ri <marian@taurux.de>
2016-03-12 22:16:29 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,31 +1,16 @@
 # expand over  raspi-conf (expand)
 # add SPI  over raspi-conf
 
-echo .
-echo "install system update"
-echo "y/n"
-read updateOS
-
-if (( $updateOS == "y" )); then
-    echo "system update"
-    sudo apt-get update
-    sudo apt-get -y upgrade
-   
-    echo .
-    echo "reboot system"
-    echo "y/n"
-    read startreboot
-
-    if (( $startreboot == "y" )); then
-        reboot
-        quit
-    fi
-fi
-
 
 echo "install needet software"
 
-sudo apt-get -y install groff
+sudo apt-get -y install groff espeak python{,3}-pifacecad python3-configobj
+sudo apt-get -y install python3-dev libmysqlclient-dev 
+sudo apt-get -y install pychess stockfish python3-espeak
+
+
+cd /opt/picofacechess
+sudo pip3 install --upgrade -r requirements.txt
 
 
 echo "get source from repositories"
@@ -36,7 +21,7 @@ sudo git clone https://github.com/marianled/dgtnix.git
 sudo chown -R  pi:pi dgtnix
 sudo git clone https://github.com/marianled/dgtdrv.git
 sudo chown -R  pi:pi dgtdrv
-sudo git clone https://github.com/marianled/picofacechess.git
+#sudo git clone https://github.com/marianled/picofacechess.git
 sudo chown -R  pi:pi picofacechess
 
 
@@ -50,3 +35,8 @@ sudo make install
 
 cd /opt/dgtdrv/
 sudo make
+
+cd /opt/picofacechess
+sudo cp /opt/picofacechess/install/picochess.init /etc/init.d/picochess
+sudo chmod +x /etc/init.d/picochess 
+sudo update-rc.d picochess defaults 98 02
