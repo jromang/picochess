6f6485c07c7c590a92ef6cf761346532f4d48fc3
Jürgen Précour <info@keyman-versicherung.info>
2016-11-11 14:56:35 +0100

    bugfix: rev2 lights

--- dgthw.py
+++ dgthw.py
@@ -93,17 +93,17 @@ class DgtHw(DgtIface):
         else:
             logging.debug('[ser] clock isnt running - no need for endText')
 
-    def light_squares_revelation_board(self, squares):
+    def light_squares_revelation_board(self, uci_move):
         if self.dgtboard.enable_revelation_leds:
-            for sq in squares:
-                dgt_square = (8 - int(sq[1])) * 8 + ord(sq[0]) - ord('a')
-                logging.debug("REV2 light on square %s", sq)
-                self.dgtboard.write_board_command([DgtCmd.DGT_SET_LEDS, 0x04, 0x01, dgt_square, dgt_square])
+            logging.debug("REV2 lights on move {}".format(uci_move))
+            fr = (8 - int(uci_move[1])) * 8 + ord(uci_move[0]) - ord('a')
+            to = (8 - int(uci_move[3])) * 8 + ord(uci_move[2]) - ord('a')
+            self.dgtboard.write([DgtCmd.DGT_SET_LEDS, 0x04, 0x01, fr, to, DgtClk.DGT_CMD_CLOCK_END_MESSAGE])
 
     def clear_light_revelation_board(self):
         if self.dgtboard.enable_revelation_leds:
             logging.debug('REV2 lights turned off')
-            self.dgtboard.write_board_command([DgtCmd.DGT_SET_LEDS, 0x04, 0x00, 0, 63])
+            self.dgtboard.write([DgtCmd.DGT_SET_LEDS, 0x04, 0x00, 0, 63, DgtClk.DGT_CMD_CLOCK_END_MESSAGE])
 
     def stop_clock(self):
         self._resume_clock(ClockSide.NONE)
