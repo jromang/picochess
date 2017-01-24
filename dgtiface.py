9b1bb7930970dfe5654fc832b80a9336dc148d80
Jürgen Précour <info@keyman-versicherung.info>
2017-01-24 16:10:34 +0100

    remove debug msg for PiCrush

--- dgtiface.py
+++ dgtiface.py
@@ -148,11 +148,11 @@ class DgtIface(DisplayDgt, Thread):
                 self.maxtimer.start()
                 logging.debug('showing {} for {} secs'.format(message, message.maxtime * self.time_factor))
                 self.maxtimer_running = True
-            logging.info('wait for msg_lock at {}'.format(self.__class__.__name__))
+            # logging.info('wait for msg_lock at {}'.format(self.__class__.__name__))
             with self.msg_lock:
-                logging.info('done for msg_lock at {}'.format(self.__class__.__name__))
+                # logging.info('done for msg_lock at {}'.format(self.__class__.__name__))
                 self._handle_message(message)
-                logging.info('handled at {}'.format(self.__class__.__name__))
+                # logging.info('handled at {}'.format(self.__class__.__name__))
         else:
             logging.debug("ignore DgtApi: {} at {}".format(message, self.__class__.__name__))
 
