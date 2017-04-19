ac2bea3187713fe0c0c4b84dd28f0543f7c2c0c4
Jürgen Precour <info@keyman-versicherung.info>
2017-04-19 08:41:31 +0200

    try with shiv's server (WS connection with special nginx config)

--- web/picoweb/static/js/app.js
+++ web/picoweb/static/js/app.js
@@ -48,7 +48,7 @@ var gameHistory, fenHash, currentPosition;
 var backend_server_prefix = 'http://drshivaji.com:3334';
 //var backend_server_prefix = "http://localhost:7777";
 // var remote_server_prefix = "localhost:8888";
-var remote_server_prefix = "drshivaji.com:5432";
+var remote_server_prefix = "drshivaji.com:9876";
 var remote_ws = null;
 
 fenHash = {};
