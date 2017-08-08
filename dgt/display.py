57f4843caaf3dadbc28f6b2ad1f57322f7021bd5
Jürgen Précour <info@keyman-versicherung.info>
2017-08-08 17:18:07 +0200

    wrong permission

--- dgt/display.py
+++ dgt/display.py
@@ -777,19 +777,20 @@ class DgtDisplay(DisplayMsg, threading.Thread):
 
         elif isinstance(message, Message.DGT_EBOARD_VERSION):
             if self.dgtmenu.inside_updt_menu():
-                logging.debug('inside menu => board channel not displayed')
+                logging.debug('inside update menu => board channel not displayed')
             else:
                 DispatchDgt.fire(message.text)
                 DispatchDgt.fire(Dgt.DISPLAY_TIME(force=True, wait=True, devs={'i2c'}))
 
         elif isinstance(message, Message.DGT_NO_EBOARD_ERROR):
             if self.dgtmenu.inside_updt_menu() or self.dgtmenu.inside_main_menu():
-                logging.debug('inside menu => board error not displayed')
+                logging.debug('inside update menu => board error not displayed')
             else:
                 DispatchDgt.fire(message.text)
 
         elif isinstance(message, Message.DGT_NO_CLOCK_ERROR):
             pass
+
         elif isinstance(message, Message.SWITCH_SIDES):
             self.engine_finished = False
             self.hint_move = chess.Move.null()
