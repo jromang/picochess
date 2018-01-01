5d56a391f9f632542daf082846689a72d2d6e7db
Jürgen Precour <info@keyman-versicherung.info>
2018-01-01 15:25:22 +0100

    prepare v0.9m

--- utilities.py
+++ utilities.py
@@ -35,7 +35,7 @@ from dgt.api import Dgt
 from configobj import ConfigObj, ConfigObjError, DuplicateError
 
 # picochess version
-version = '09l'
+version = '09m'
 
 evt_queue = queue.Queue()
 dispatch_queue = queue.Queue()
